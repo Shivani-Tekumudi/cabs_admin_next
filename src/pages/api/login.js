4fcc818c560433ae0fe6b0437de00c19386a506b
Shivani-Tekumudi <shivani9668@gmail.com>
2025-07-10 18:19:55 +0530

    changes in loginHandler

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,7 +1,19 @@
+import admins from '@/data/admins.json';
+
+
 export default function loginHandler(req, res){
 const {email,password} = req.body;
+const admin = admins.find(a => a.email === email  && a.pswd === password)
+
+
+
+console.log('Incoming request body:', req.body);
+  console.log('Admins:', admins); 
+  console.log('find admin' , admin)
 if (req.method === 'POST') {
-if(email === 'admin1@gmail.com' && password === '123456'){
+
+
+if(admin){
     const ONE_DAY = 60 * 60 * 24;
     res.setHeader('Set-cookie', `loggedIn=true;Max-Age=${ONE_DAY}; Path=/ ; HttpOnly;Secure; SameSite=Lax`);
     return res.status(200).json({ message: 'Login successful' })
