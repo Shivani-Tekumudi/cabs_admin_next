e4fb179e0fbaa44037c8fa99dc9a7dc0fce383f2
Shivani-Tekumudi <shivani9668@gmail.com>
2025-07-11 12:39:57 +0530

    final commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Login</h2>
         <form className="space-y-5" onSubmit={handleLogin}>
           <div>
-            <label  className="block text-gray-700 text-sm font-semibold mb-2">Username:</label>
+            <label  className="block text-gray-700 text-sm font-semibold mb-2">Email:</label>
             <input type="email" id="username" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200" placeholder="Enter username (e.g., user)" value={email} onChange={(e) => setEmail(e.target.value)}  required />
           </div>
           <div>
@@ -62,7 +62,7 @@ export default function Home() {
         </form>
 
  {error && <p className="text-red-500 text-sm mt-3 text-center">{error}</p>}
-        <p className="text-center text-gray-500 text-sm mt-6">Hint: Username is `user` and Password is `password`  </p></div>
+        <p className="text-center text-gray-500 text-sm mt-6">Hint: email is `admin[1/2/3/4/5/6]@gmail.com` and Password is `123456`  </p></div>
 
     </div>
   );
